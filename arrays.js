44abd6ca9864f58b5f99d905026b3e48de154bfe
Felix Martinez <felixaviermartinez@gmail.com>
2019-06-27 20:02:57 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,41 @@
+var chocolateBars = [ 'snickers', 'hundred grand', 'kitkat', 'skittles' ];
+
+function addElementToBeginningOfArray(array, element) {
+  myArray = ["foo"];
+  return [...myArray, 1];
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  myArray = array;
+  myArray.unshift("foo");
+  return myArray;
+}
+
+function addElementToEndOfArray(array, element) {
+  myArray = [1];
+  return [...myArray, "foo"];
+}
+
+function destructivelyAddElementToEndOfArray(array, element) {
+  myArray = array;
+  myArray.push("foo");
+  return myArray;
+}
+
+var entrepreneurs = ["Oprah Winfrey", "Lauren Powell Jobs", "Arianna Huffington", "Bill Gates"];
+
+function accessElementInArray(array, index) {
+  return array[3];
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(array) {
+  
+}
+
+function DestructivelyRemoveElementFromEndOfArray(array) {
+  
+}
+
+function removeElementFromEndOfArray(array) {
+  
+}
